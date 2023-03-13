b4d0247bb07aaca1f4310157e8673ed9c7b99740
Dhanush <dhanushbommavaram@gmail.com>
2023-03-13 09:16:52 +0530

    added adapter for video_item with only urls

--- ShortVideoApp/app/src/main/java/com/example/shortvideoapp/adapter/VideoItemAdapter.kt
+++ ShortVideoApp/app/src/main/java/com/example/shortvideoapp/adapter/VideoItemAdapter.kt
@@ -17,17 +17,10 @@ import com.example.shortvideoapp.model.Video
 
 class VideoItemAdapter(private val context: Context, val dataset:MutableList<Video>): RecyclerView.Adapter<VideoItemAdapter.ItemViewHolder>()
 {
-        // Provide a reference to the views for each data item
-        // Complex data items may need more than one view per item, and
-        // you provide access to all the views for a data item in a view holder.
-        // Each data item is just an Affirmation object.
     class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         val videoView: VideoView = view.findViewById(R.id.videoView)
     }
 
-    /**
-     * Create new views (invoked by the layout manager)
-     */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         // create a new view
         val adapterLayout = LayoutInflater.from(parent.context)
@@ -36,30 +29,10 @@ class VideoItemAdapter(private val context: Context, val dataset:MutableList<Vid
         return ItemViewHolder(adapterLayout)
     }
 
-    /**
-     * Replace the contents of a view (invoked by the layout manager)
-     */
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
-        holder.videoView.setVideoPath(item.videoURL)
 
-//        val mediaController = MediaController(context)
-//        mediaController.visibility = View.GONE;
-//        // on below line we are setting anchor
-//        // view for our media controller.
-//        mediaController.setAnchorView(holder.videoView)
-//
-//        // on below line we are setting media player
-//        // for our media controller.
-//        mediaController.setMediaPlayer(holder.videoView)
-//
-//        // on below line we are setting media
-//        // controller for our video view.
-//        holder.videoView.setMediaController(mediaController)
-//
-//        // on below line we are
-//        // simply starting our video view.
-//        holder.videoView.start()
+        holder.videoView.setVideoPath(item.videoURL)
         holder.videoView.setOnPreparedListener{ mp ->
             val videoRatio = mp.videoWidth / mp.videoHeight.toFloat()
             val screenRatio = holder.videoView.width / holder.videoView.height.toFloat()
