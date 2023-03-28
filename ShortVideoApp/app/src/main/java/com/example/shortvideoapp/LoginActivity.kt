3fcc25ac70d4f9810a7aa247f676df9a8f528eef
Dhanush <dhanushbommavaram@gmail.com>
2023-03-28 09:05:57 +0530

    Camel cased view names

--- ShortVideoApp/app/src/main/java/com/example/shortvideoapp/LoginActivity.kt
+++ ShortVideoApp/app/src/main/java/com/example/shortvideoapp/LoginActivity.kt
@@ -2,10 +2,21 @@ package com.example.shortvideoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.OnBackPressedDispatcher
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val s:EditText=findViewById(R.id.username_input)
+
+    }
+
+    @Deprecated("Deprecated in Java", ReplaceWith("moveTaskToBack(true)"))
+    override fun onBackPressed() {
+        moveTaskToBack(true)
     }
 }
