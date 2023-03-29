09e646095c468eebb579cba054dec1dafc75fd8a
Excess <srinath.bellamkonda@gmail.com>
2023-03-30 04:58:15 +0530

    changed where login takes you

--- ShortVideoApp/app/src/main/java/com/example/shortvideoapp/LoginActivity.kt
+++ ShortVideoApp/app/src/main/java/com/example/shortvideoapp/LoginActivity.kt
@@ -65,7 +65,7 @@ class LoginActivity : AppCompatActivity() {
         // Check if user is signed in (non-null) and update UI accordingly.
         val currentUser = auth.currentUser
         if(currentUser != null){
-            Intent(this,MainActivity::class.java).also{
+            Intent(this,UploadActivity::class.java).also{
                 startActivity(it)
             }
         }
