e99b7d49acb4e9119ec1f4f4b6d9dab1b73bcdbb
Pranav <maddipranavreddy@gmail.com>
2023-04-18 23:47:42 +0530

    Added some  more features in Exception handling

--- ShortVideoApp/app/src/main/java/com/example/shortvideoapp/SignupActivity.kt
+++ ShortVideoApp/app/src/main/java/com/example/shortvideoapp/SignupActivity.kt
@@ -7,12 +7,10 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
 import com.google.firebase.auth.FirebaseAuthInvalidUserException
 import com.google.firebase.auth.ktx.auth
-
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.ktx.Firebase
 import java.util.*
 
@@ -51,6 +49,14 @@ class SignupActivity : AppCompatActivity() {
             return
         }
 
+        if (password.text.toString().length < 4) {
+            Toast.makeText(
+                this, "Password should be at least 4 characters long.",
+                Toast.LENGTH_SHORT
+            ).show()
+            return
+        }
+        email.text.removeSurrounding(" ");
         auth.createUserWithEmailAndPassword(email.text.toString(),password.text.toString()).addOnCompleteListener(this) { task ->
             if (task.isSuccessful) {
                 Toast.makeText(
@@ -59,56 +65,35 @@ class SignupActivity : AppCompatActivity() {
                 ).show()
                 finish()
             } else {
-                try {
-                    throw task.exception!!
-                } catch (e: FirebaseAuthInvalidCredentialsException) {
-                    Toast.makeText(
-                        this, AuthExceptionHandler.generateExceptionMessage(AuthExceptionHandler.AuthResultStatus.invalidEmail),
-                        Toast.LENGTH_SHORT
-                    ).show()
-                } catch (e: FirebaseAuthInvalidUserException) {
-                    Toast.makeText(
-                        this, AuthExceptionHandler.generateExceptionMessage(AuthExceptionHandler.AuthResultStatus.userNotFound),
-                        Toast.LENGTH_SHORT
-                    ).show()
-                } catch (e: Exception) {
-                    Toast.makeText(
-                        this, AuthExceptionHandler.generateExceptionMessage(AuthExceptionHandler.AuthResultStatus.undefined),
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
+                val errorCode:String= (task.exception as FirebaseAuthException?)!!.errorCode;
+                Toast.makeText(
+                    this, signupErrorToast(errorCode), Toast.LENGTH_SHORT
+                ).show()
             }
         }
     }
 }
 
-class AuthExceptionHandler {
-    enum class AuthResultStatus {
-        invalidEmail,
-        wrongPassword,
-        userNotFound,
-        userDisabled,
-        tooManyRequests,
-        operationNotAllowed,
-        emailAlreadyExists,
-        undefined
-    }
-
-    companion object {
-        fun handleException(e: Exception): AuthResultStatus {
-            return when (e) {
-                is FirebaseAuthInvalidCredentialsException -> AuthResultStatus.invalidEmail
-                is FirebaseAuthInvalidUserException -> AuthResultStatus.userNotFound
-                else -> AuthResultStatus.undefined
-            }
-        }
-
-        fun generateExceptionMessage(exceptionCode: AuthResultStatus): String {
-            return when (exceptionCode) {
-                AuthResultStatus.invalidEmail -> "Your email address appears to be malformed."
-                AuthResultStatus.userNotFound -> "User with this email doesn't exist."
-                else -> "An undefined Error happened."
-            }
-        }
-    }
+fun signupErrorToast(errorCode:String):String
+{
+    return when(errorCode){
+        "ERROR_INVALID_CUSTOM_TOKEN" -> "The custom token format is incorrect."
+        "ERROR_CUSTOM_TOKEN_MISMATCH"-> "The custom token corresponds to a different audience."
+        "ERROR_INVALID_CREDENTIAL"->  "The supplied auth credential is malformed or has expired."
+        "ERROR_INVALID_EMAIL"-> "The email address is badly formatted."
+        "ERROR_WRONG_PASSWORD"-> "The password is invalid or the user does not have a password."
+        "ERROR_USER_MISMATCH"-> "The supplied credentials do not correspond to the previously signed in user."
+        "ERROR_REQUIRES_RECENT_LOGIN"-> "This operation is sensitive and requires recent authentication. Log in again before retrying this request."
+        "ERROR_ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL"-> "An account already exists with the same email address but different sign-in credentials. Sign in using a provider associated with this email address."
+        "ERROR_EMAIL_ALREADY_IN_USE"-> "The email address is already in use by another account."
+        "ERROR_CREDENTIAL_ALREADY_IN_USE"-> "This credential is already associated with a different user account."
+        "ERROR_USER_DISABLED"-> "The user account has been disabled by an administrator."
+        "ERROR_USER_TOKEN_EXPIRED"-> "The user's credential is no longer valid. The user must sign in again."
+        "ERROR_USER_NOT_FOUND"-> "There is no user record corresponding to this identifier. The user may have been deleted."
+        "ERROR_INVALID_USER_TOKEN"-> "The user's credential is no longer valid. The user must sign in again."
+        "ERROR_OPERATION_NOT_ALLOWED"-> "This operation is not allowed. You must enable this service in the console."
+        "ERROR_WEAK_PASSWORD"-> "The given password is invalid."
+        "ERROR_MISSING_EMAIL"-> "An email address must be provided."
+        else -> {""}
+    }.toString()
 }
