b5e1c322ae9e7e21a8e936d9a6578d5a6983483b
nimishasingh14 <nimishasingh14k@gmail.com>
2023-04-19 20:12:43 +0530

    First Commit

--- ShortVideoApp/app/src/main/java/com/example/shortvideoapp/model/Post.kt
+++ ShortVideoApp/app/src/main/java/com/example/shortvideoapp/model/Post.kt
@@ -1,6 +1,10 @@
 package com.example.shortvideoapp.model
 
-data class Post(val videoURL:String,val username:String,val title:String,val description:String,val upvotes:Int,val downvotes:Int)
-{
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
+data class Post(val videoURL:String,val uid:String,val title:String,val description:String,val upvotes:Int,val downvotes:Int)
+{
+    private val databaseUsername: DatabaseReference = FirebaseDatabase.getInstance("https://shortvideoapp-e7456-default-rtdb.asia-southeast1.firebasedatabase.app/").reference.child("users").child(uid);
+    val username:String=databaseUsername.getValue(String.class);
 }
