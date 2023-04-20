fca11e85a15e062b8ed9fec9aaf1038228d14bbe
Dhanush <dhanushbommavaram@gmail.com>
2023-04-20 11:25:53 +0530

    data class post

--- ShortVideoApp/app/src/main/java/com/example/shortvideoapp/model/Post.kt
+++ ShortVideoApp/app/src/main/java/com/example/shortvideoapp/model/Post.kt
@@ -1,10 +1,23 @@
 package com.example.shortvideoapp.model
 
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import android.util.Log
+import com.google.firebase.database.*
 
 data class Post(val videoURL:String,val uid:String,val title:String,val description:String,val upvotes:Int,val downvotes:Int)
 {
-    private val databaseUsername: DatabaseReference = FirebaseDatabase.getInstance("https://shortvideoapp-e7456-default-rtdb.asia-southeast1.firebasedatabase.app/").reference.child("users").child(uid);
-    val username:String=databaseUsername.getValue(String.class);
+    private val databaseUsername: DatabaseReference = FirebaseDatabase.getInstance("https://shortvideoapp-e7456-default-rtdb.asia-southeast1.firebasedatabase.app/").reference.child("users").child(uid).child("username");
+    var username:String?=null;
+    init {
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                username = dataSnapshot.value as String;
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Getting Post failed, log a message
+//                Log.w(TAG, "loadPost:onCancelled", databaseError.toException())
+            }
+        }
+        databaseUsername.addValueEventListener(postListener)
+    }
 }
