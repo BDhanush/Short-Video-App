48db2225727d811d61a3286a3b36bd5a248f1410
Pranav <maddipranavreddy@gmail.com>
2023-04-20 15:36:49 +0530

    Exception Handling

--- ShortVideoApp/app/src/main/java/com/example/shortvideoapp/MainActivity.kt
+++ ShortVideoApp/app/src/main/java/com/example/shortvideoapp/MainActivity.kt
@@ -15,9 +15,9 @@ class MainActivity : AppCompatActivity() {
         val videosViewPager:ViewPager2 = findViewById<ViewPager2>(R.id.viewPagerVideos)
 
         val videosURlList= mutableListOf<Post>()
-        videosURlList.add(Post("https://cdn.discordapp.com/attachments/910134893151911946/1056972395543527474/iam-like-what-he-say-fuck-me-for-50-cent-talking-about-floyd-mayweather-reaction-video-meme-vidownload.mp4"));
-        videosURlList.add(Post("https://cdn.discordapp.com/attachments/765130391119593482/1081900110121807872/chatgpt.mp4"));
-        videosURlList.add(Post("https://cdn.discordapp.com/attachments/910134893151911946/1021783588187938826/VID-20220408-WA0001.mp4"));
+        videosURlList.add(Post("https://cdn.discordapp.com/attachments/910134893151911946/1056972395543527474/iam-like-what-he-say-fuck-me-for-50-cent-talking-about-floyd-mayweather-reaction-video-meme-vidownload.mp4", "", "", "", 0, 0));
+        videosURlList.add(Post("https://cdn.discordapp.com/attachments/765130391119593482/1081900110121807872/chatgpt.mp4", "", "", "", 0, 0));
+        videosURlList.add(Post("https://cdn.discordapp.com/attachments/910134893151911946/1021783588187938826/VID-20220408-WA0001.mp4", "", "", "", 0, 0));
         videosViewPager.adapter = VideoItemAdapter(this,videosURlList);
 
 
--- ShortVideoApp/app/src/main/java/com/example/shortvideoapp/SignupActivity.kt
+++ ShortVideoApp/app/src/main/java/com/example/shortvideoapp/SignupActivity.kt
@@ -20,12 +20,12 @@ class SignupActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
 
-        val signupButton:Button=findViewById(R.id.signupButton)
-        val loginButton:Button=findViewById(R.id.loginButton)
+        val signupButton: Button = findViewById(R.id.signupButton)
+        val loginButton: Button = findViewById(R.id.loginButton)
 
         loginButton.setOnClickListener {
             finish()
-            Intent(this,LoginActivity::class.java).also{
+            Intent(this, LoginActivity::class.java).also {
                 startActivity(it)
             }
         }
@@ -33,13 +33,30 @@ class SignupActivity : AppCompatActivity() {
             signup()
         }
     }
+
     private fun signup() {
-        val email:EditText = findViewById(R.id.emailInput)
-        val password:EditText = findViewById(R.id.passwordInput)
-        val confirmPassword:EditText = findViewById(R.id.confirmPasswordInput)
-        val username:EditText = findViewById(R.id.usernameInput)
+        val email: EditText = findViewById(R.id.emailInput)
+        val password: EditText = findViewById(R.id.passwordInput)
+        val confirmPassword: EditText = findViewById(R.id.confirmPasswordInput)
+        val username: EditText = findViewById(R.id.usernameInput)
 
         auth = Firebase.auth
+
+        if (email.text == null || email.text.toString().isEmpty()) {
+            Toast.makeText(
+                this, "Please enter an email address.",
+                Toast.LENGTH_SHORT
+            ).show()
+            return
+        }
+
+        if (username.text.toString() == password.text.toString()) {
+            Toast.makeText(
+                this, "Username and password cannot be the same.",
+                Toast.LENGTH_SHORT
+            ).show()
+            return
+        }
 
         if (password.text.toString() != confirmPassword.text.toString()) {
             Toast.makeText(
@@ -56,44 +73,47 @@ class SignupActivity : AppCompatActivity() {
             ).show()
             return
         }
-        email.text.removeSurrounding(" ");
-        auth.createUserWithEmailAndPassword(email.text.toString(),password.text.toString()).addOnCompleteListener(this) { task ->
-            if (task.isSuccessful) {
-                Toast.makeText(
-                    this, "Authentication successful.",
-                    Toast.LENGTH_SHORT
-                ).show()
-                finish()
-            } else {
-                val errorCode:String= (task.exception as FirebaseAuthException?)!!.errorCode;
-                Toast.makeText(
-                    this, signupErrorToast(errorCode), Toast.LENGTH_SHORT
-                ).show()
-            }
-        }
-    }
-}
 
-fun signupErrorToast(errorCode:String):String
-{
-    return when(errorCode){
-        "ERROR_INVALID_CUSTOM_TOKEN" -> "The custom token format is incorrect."
-        "ERROR_CUSTOM_TOKEN_MISMATCH"-> "The custom token corresponds to a different audience."
-        "ERROR_INVALID_CREDENTIAL"->  "The supplied auth credential is malformed or has expired."
-        "ERROR_INVALID_EMAIL"-> "The email address is badly formatted."
-        "ERROR_WRONG_PASSWORD"-> "The password is invalid or the user does not have a password."
-        "ERROR_USER_MISMATCH"-> "The supplied credentials do not correspond to the previously signed in user."
-        "ERROR_REQUIRES_RECENT_LOGIN"-> "This operation is sensitive and requires recent authentication. Log in again before retrying this request."
-        "ERROR_ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL"-> "An account already exists with the same email address but different sign-in credentials. Sign in using a provider associated with this email address."
-        "ERROR_EMAIL_ALREADY_IN_USE"-> "The email address is already in use by another account."
-        "ERROR_CREDENTIAL_ALREADY_IN_USE"-> "This credential is already associated with a different user account."
-        "ERROR_USER_DISABLED"-> "The user account has been disabled by an administrator."
-        "ERROR_USER_TOKEN_EXPIRED"-> "The user's credential is no longer valid. The user must sign in again."
-        "ERROR_USER_NOT_FOUND"-> "There is no user record corresponding to this identifier. The user may have been deleted."
-        "ERROR_INVALID_USER_TOKEN"-> "The user's credential is no longer valid. The user must sign in again."
-        "ERROR_OPERATION_NOT_ALLOWED"-> "This operation is not allowed. You must enable this service in the console."
-        "ERROR_WEAK_PASSWORD"-> "The given password is invalid."
-        "ERROR_MISSING_EMAIL"-> "An email address must be provided."
-        else -> {""}
-    }.toString()
+        auth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(
+                        this, "Authentication successful.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    finish()
+                } else {
+                    val errorCode: String = (task.exception as FirebaseAuthException?)!!.errorCode;
+                    Toast.makeText(
+                        this, signupErrorToast(errorCode), Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
+
+
+    fun signupErrorToast(errorCode: String): String {
+        return when (errorCode) {
+            "ERROR_INVALID_CUSTOM_TOKEN" -> "The custom token format is incorrect."
+            "ERROR_CUSTOM_TOKEN_MISMATCH" -> "The custom token corresponds to a different audience."
+            "ERROR_INVALID_CREDENTIAL" -> "The supplied auth credential is malformed or has expired."
+            "ERROR_INVALID_EMAIL" -> "The email address is badly formatted."
+            "ERROR_WRONG_PASSWORD" -> "The password is invalid or the user does not have a password."
+            "ERROR_USER_MISMATCH" -> "The supplied credentials do not correspond to the previously signed in user."
+            "ERROR_REQUIRES_RECENT_LOGIN" -> "This operation is sensitive and requires recent authentication. Log in again before retrying this request."
+            "ERROR_ACCOUNT_EXISTS_WITH_DIFFERENT_CREDENTIAL" -> "An account already exists with the same email address but different sign-in credentials. Sign in using a provider associated with this email address."
+            "ERROR_EMAIL_ALREADY_IN_USE" -> "The email address is already in use by another account."
+            "ERROR_CREDENTIAL_ALREADY_IN_USE" -> "This credential is already associated with a different user account."
+            "ERROR_USER_DISABLED" -> "The user account has been disabled by an administrator."
+            "ERROR_USER_TOKEN_EXPIRED" -> "The user's credential is no longer valid. The user must sign in again."
+            "ERROR_USER_NOT_FOUND" -> "There is no user record corresponding to this identifier. The user may have been deleted."
+            "ERROR_INVALID_USER_TOKEN" -> "The user's credential is no longer valid. The user must sign in again."
+            "ERROR_OPERATION_NOT_ALLOWED" -> "This operation is not allowed. You must enable this service in the console."
+            "ERROR_WEAK_PASSWORD" -> "The given password is weak."
+            "ERROR_MISSING_EMAIL" -> "An email address must be provided."
+            else -> {
+                ""
+            }
+        }.toString()
+    }
 }
