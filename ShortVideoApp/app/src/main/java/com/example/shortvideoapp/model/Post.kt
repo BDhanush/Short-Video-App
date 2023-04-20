5d1aafabdcf21a5de83396ba9bede5f6f19b2f3b
Dhanush <dhanushbommavaram@gmail.com>
2023-04-20 11:28:36 +0530

    data class post

--- ShortVideoApp/app/src/main/java/com/example/shortvideoapp/model/Post.kt
+++ ShortVideoApp/app/src/main/java/com/example/shortvideoapp/model/Post.kt
@@ -5,9 +5,10 @@ import com.google.firebase.database.*
 
 data class Post(val videoURL:String,val uid:String,val title:String,val description:String,val upvotes:Int,val downvotes:Int)
 {
-    private val databaseUsername: DatabaseReference = FirebaseDatabase.getInstance("https://shortvideoapp-e7456-default-rtdb.asia-southeast1.firebasedatabase.app/").reference.child("users").child(uid).child("username");
     var username:String?=null;
+    val comments:MutableList<String> = mutableListOf();
     init {
+        val databaseUsername: DatabaseReference = FirebaseDatabase.getInstance("https://shortvideoapp-e7456-default-rtdb.asia-southeast1.firebasedatabase.app/").reference.child("users").child(uid).child("username");
         val postListener = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 username = dataSnapshot.value as String;
