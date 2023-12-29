69079b89a4998019e09a2e19283522dc446b3d1c
Dhanush <dhanushbommavaram@gmail.com>
2023-12-30 00:58:40 +0530

    click to open post search results

--- ShortVideoApp/app/src/main/java/com/example/shortvideoapp/adapter/SearchAdapter.kt
+++ ShortVideoApp/app/src/main/java/com/example/shortvideoapp/adapter/SearchAdapter.kt
@@ -1,12 +1,17 @@
 package com.example.shortvideoapp.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.*
 import android.widget.*
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.shortvideoapp.DisplayVideoActivity
 import com.example.shortvideoapp.R
 import com.example.shortvideoapp.model.Post
+import com.google.android.material.card.MaterialCardView
+import kotlinx.coroutines.NonDisposableHandle.parent
 
 class SearchAdapter(context: Context, var dataset:MutableList<Post>): RecyclerView.Adapter<SearchAdapter.ItemViewHolder>()
 {
@@ -15,6 +20,7 @@ class SearchAdapter(context: Context, var dataset:MutableList<Post>): RecyclerVi
     inner class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         val thumbnail:ImageView=view.findViewById(R.id.thumbnail)
         val title:TextView=view.findViewById(R.id.title)
+        val post:ConstraintLayout=view.findViewById(R.id.post)
 
 
     }
@@ -31,7 +37,11 @@ class SearchAdapter(context: Context, var dataset:MutableList<Post>): RecyclerVi
         val item = dataset[position]
         Glide.with(context).load(item.thumbnail).into(holder.thumbnail)
         holder.title.text=item.title
-
+        holder.post.setOnClickListener{
+            val intent = Intent(context, DisplayVideoActivity::class.java)
+            intent.putExtra("postKey", dataset[position].key)
+            context.startActivity(intent)
+        }
 
     }
 
