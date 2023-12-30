d81503bafbcc82bf7850c7c913e40ec84c562e11
Dhanush <dhanushbommavaram@gmail.com>
2023-12-30 08:44:15 +0530

    search uncomment

--- ShortVideoApp/app/src/main/java/com/example/shortvideoapp/SearchActivity.kt
+++ ShortVideoApp/app/src/main/java/com/example/shortvideoapp/SearchActivity.kt
@@ -132,18 +132,18 @@ class SearchActivity : AppCompatActivity() {
                     val post = postFromMap(postMap)
                     post.key = snapshot.key as String
 
-//                    if (post.title?.contains(searchPrefix, ignoreCase = true) == true ||
-//                        post.tags?.contains(searchPrefix, ignoreCase = true) == true) {
-//                        filteredList.add(post)
-//                    } else {
-//                        val tags = post.tags?.split(",")?.map { it.trim() } ?: listOf()
-//                        for (tag in tags) {
-//                            if (tag.contains(searchPrefix, ignoreCase = true)) {
-//                                filteredList.add(post)
-//                                break
-//                            }
-//                        }
-//                    }
+                    if (post.title?.contains(searchPrefix, ignoreCase = true) == true ||
+                        post.tags?.contains(searchPrefix, ignoreCase = true) == true) {
+                        filteredList.add(post)
+                    } else {
+                        val tags = post.tags?.split(",")?.map { it.trim() } ?: listOf()
+                        for (tag in tags) {
+                            if (tag.contains(searchPrefix, ignoreCase = true)) {
+                                filteredList.add(post)
+                                break
+                            }
+                        }
+                    }
                 }
 
                 adapter = SearchAdapter(applicationContext, filteredList)
